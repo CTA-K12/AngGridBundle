58ae7016665afc77dcfe48b8984e493cdf7f8060
lighthart <dlighthart@mesd.k12.or.us>
2013-08-19 11:08:06 -0700

    Removed grid filter

--- Resources/public/js/grid_filters.js
+++ Resources/public/js/grid_filters.js
@@ -1,12 +1,1 @@
 'use strict';
-
-angular.module('gridFilters', [])
-    .filter('boolean' , function() {
-        return function(input) {
-            console.log("Input");
-            console.log(input);
-            return ('true' == input) ? 'true-check' : 'false-ex';
-        };
-    });
-
-
