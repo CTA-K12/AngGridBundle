d5d28a967502013b8d90acfe989eb007fc8986cc
lighthart <dlighthart@mesd.k12.or.us>
2013-09-09 10:39:54 -0700

    Updates to align

--- Helper/GridManager.php
+++ Helper/GridManager.php
@@ -39,16 +39,16 @@ class GridManager {
         $this->grid['exportString'] = $this->request->query->get( 'exportString' );
         $this->grid['headers'] = array();
         $this->grid['requestCount'] = $this->request->query->get( 'requestCount' );
-        $this->grid['exportArray'] = is_null($snappy)
+        $this->grid['exportArray'] = is_null( $snappy )
             ? array(
-                array('label' => 'TSV', 'value' => 'tsv', 'exportLink' => '#'),
-                array('label' => 'CSV', 'value' => 'csv', 'exportLink' => '#'),
-                array('label' => 'Excel', 'value' => 'xls', 'exportLink' => '#') )
+            array( 'label' => 'TSV', 'value' => 'tsv', 'exportLink' => '#' ),
+            array( 'label' => 'CSV', 'value' => 'csv', 'exportLink' => '#' ),
+            array( 'label' => 'Excel', 'value' => 'xls', 'exportLink' => '#' ) )
             : array(
-                array('label' => 'TSV', 'value' => 'tsv', 'exportLink' => '#'),
-                array('label' => 'CSV', 'value' => 'csv', 'exportLink' => '#'),
-                array('label' => 'Excel', 'value' => 'xls', 'exportLink' => '#'),
-                array('label' => 'PDF', 'value' => 'pdf', 'exportLink' => '#') );
+            array( 'label' => 'TSV', 'value' => 'tsv', 'exportLink' => '#' ),
+            array( 'label' => 'CSV', 'value' => 'csv', 'exportLink' => '#' ),
+            array( 'label' => 'Excel', 'value' => 'xls', 'exportLink' => '#' ),
+            array( 'label' => 'PDF', 'value' => 'pdf', 'exportLink' => '#' ) );
 
         if ( is_null( $this->grid['exportString'] ) ) {
             $this->export = false;
@@ -56,74 +56,74 @@ class GridManager {
             $this->export = true;
         }
 
-        $grid0 = $this->request->cookies->get('grid0');
-        if (isset($grid0)) {
-            $cookie =  json_decode($grid0);
+        $grid0 = $this->request->cookies->get( 'grid0' );
+        if ( isset( $grid0 ) ) {
+            $cookie =  json_decode( $grid0 );
         }
         $addView = $this->request->query->get( 'addView' );
-        if (isset($addView)) {
+        if ( isset( $addView ) ) {
             $this->grid['addView'] = $addView;
         } else {
-            $this->grid['addView'] = json_decode($this->request->cookies->get('addView'));
-            if (isset($cookie->addView)) {
+            $this->grid['addView'] = json_decode( $this->request->cookies->get( 'addView' ) );
+            if ( isset( $cookie->addView ) ) {
                 $this->grid['addView'] = $cookie->addView;
             }
         }
         $page = $this->request->query->get( 'page' );
-        if (isset($page)) {
+        if ( isset( $page ) ) {
             $this->grid['page'] = $page;
         } else {
-            $this->grid['page'] = json_decode($this->request->cookies->get('page'));
-            if (isset($cookie->page)) {
+            $this->grid['page'] = json_decode( $this->request->cookies->get( 'page' ) );
+            if ( isset( $cookie->page ) ) {
                 $this->grid['page'] = $cookie->page;
             }
         }
         $perPage = $this->request->query->get( 'perPage' );
-        if (isset($perPage)) {
+        if ( isset( $perPage ) ) {
             $this->grid['perPage'] = $perPage;
         } else {
-            $this->grid['perPage'] = json_decode($this->request->cookies->get('perPage'));
-            if (isset($cookie->perPage)) {
+            $this->grid['perPage'] = json_decode( $this->request->cookies->get( 'perPage' ) );
+            if ( isset( $cookie->perPage ) ) {
                 $this->grid['perPage'] = $cookie->perPage;
             }
         }
         $search = $this->request->query->get( 'search' );
-        if (isset($search)) {
+        if ( isset( $search ) ) {
             $this->grid['search'] = $search;
         } else {
-            $this->grid['search'] = json_decode($this->request->cookies->get('search'));
-            if (isset($cookie->search)) {
+            $this->grid['search'] = json_decode( $this->request->cookies->get( 'search' ) );
+            if ( isset( $cookie->search ) ) {
                 $this->grid['search'] = $cookie->search;
             }
         }
         $showControl = $this->request->query->get( 'showControl' );
-        if (isset($showControl)) {
+        if ( isset( $showControl ) ) {
             $this->grid['showControl'] = $showControl;
         } else {
-            $this->grid['showControl'] = json_decode($this->request->cookies->get('showControl'));
-            if (isset($cookie->showControl)) {
+            $this->grid['showControl'] = json_decode( $this->request->cookies->get( 'showControl' ) );
+            if ( isset( $cookie->showControl ) ) {
                 $this->grid['showControl'] = $cookie->showControl;
             }
         }
-        $sorts = json_decode($this->request->query->get( 'sorts' ));
-        if (isset($sorts)) {
+        $sorts = json_decode( $this->request->query->get( 'sorts' ) );
+        if ( isset( $sorts ) ) {
             $this->grid['sorts'] = $sorts;
         } else {
-            $this->grid['sorts'] = json_decode($this->request->cookies->get('sorts'));
-            if (isset($cookie->sorts)) {
+            $this->grid['sorts'] = json_decode( $this->request->cookies->get( 'sorts' ) );
+            if ( isset( $cookie->sorts ) ) {
                 $this->grid['sorts'] = $cookie->sorts;
             }
         }
 
-        if (!isset($this->grid['addView'])) {
+        if ( !isset( $this->grid['addView'] ) ) {
             $this->grid['addView'] = false;
         }
 
-        if (!isset($this->grid['showControl'])) {
+        if ( !isset( $this->grid['showControl'] ) ) {
             $this->grid['showControl'] = true;
         }
 
-        if (!isset($this->grid['sorts'])) {
+        if ( !isset( $this->grid['sorts'] ) ) {
             $this->grid['sorts'] = null;
         }
     }
@@ -193,7 +193,7 @@ class GridManager {
             $name = $item['field'];
             if ( !isset( $item['column'] ) ) {
                 $last = strrpos( $name, '.' );
-                if (false == $last) {
+                if ( false == $last ) {
                     $item['column'] = $name;
                 } else {
                     $nextLast = strrpos( $name, '.', $last - strlen( $name ) - 1 );
@@ -204,6 +204,19 @@ class GridManager {
                     }
                 }
             }
+        }
+
+        if ( isset( $item['align'] ) && 'right' == $item['align'] ) {
+            $item['align'] = 'td-align-right';
+        }
+
+        if ( isset( $item['align'] ) && 'center' == $item['align'] ) {
+            $item['align'] = 'td-align-center';
+        }
+
+        // or for default case
+        if ( !isset( $item['align'] ) || 'left' == $item['align'] ) {
+            $item['align'] = 'td-align-left';
         }
 
         if ( !isset( $item['header'] ) ) {
@@ -263,7 +276,7 @@ class GridManager {
         $this->grid['headers'] = Query::hideColumns( $this->grid['headers'], $columns );
     }
 
-    public function getJsonResponse($distinct = true) {
+    public function getJsonResponse( $distinct = true ) {
         $this->queryBuilder->select( $this->queryBuilder->expr()->count( 'distinct ' . $this->root . '.id' ) );
         $this->grid['total'] = $this->queryBuilder->getQuery()->getSingleScalarResult();
         $search = $this->prepend.$this->grid['search'];
@@ -271,16 +284,16 @@ class GridManager {
         $this->grid['filtered'] = $this->queryBuilder->getQuery()->getSingleScalarResult();
         $this->queryBuilder->select( $this->root );
 
-        if (0 < $this->grid['filtered']) {
+        if ( 0 < $this->grid['filtered'] ) {
 
             foreach ( $this->selects as $select ) {
                 $this->queryBuilder->addSelect( $select );
             }
 
-            if (is_null($this->grid['page'])) {
+            if ( is_null( $this->grid['page'] ) ) {
                 $this->grid['page'] = 1;
             }
-            if (is_null($this->grid['perPage'])) {
+            if ( is_null( $this->grid['perPage'] ) ) {
                 $this->grid['perPage'] = $this->grid['filtered'];
             }
 
@@ -304,7 +317,7 @@ class GridManager {
         }
 
         if ( $this->export ) {
-            if ($this->grid['exportType'] == 'pdf' && !is_null($this->snappy)) {
+            if ( $this->grid['exportType'] == 'pdf' && !is_null( $this->snappy ) ) {
                 $html = $this->controller->render( 'MESDAngGridBundle:Grid:export.pdf.twig',
                     array(
                         'entities' => $this->grid['entities'],
@@ -312,10 +325,10 @@ class GridManager {
                     )
                 );
 
-                $response = new Response($this->snappy->getOutputFromHtml($html, array('orientation' => 'Landscape',
-                        'print-media-type' => true,
-                        'footer-left'  => 'Exported on [date] at [time]',
-                        'footer-right' => 'Page [page] of [toPage]')),
+                $response = new Response( $this->snappy->getOutputFromHtml( $html, array( 'orientation' => 'Landscape',
+                            'print-media-type' => true,
+                            'footer-left'  => 'Exported on [date] at [time]',
+                            'footer-right' => 'Page [page] of [toPage]' ) ),
                     200,
                     array(
                         'Content-Type'          => 'application/pdf',
@@ -324,12 +337,12 @@ class GridManager {
                 );
             }
             else {
-                $response = new Response($this->controller->render( 'MESDAngGridBundle:Grid:export.' . $this->grid['exportType'] . '.twig',
-                    array(
-                        'entities' => $this->grid['entities'],
-                        'headers' => $this->grid['headers'],
-                    )
-                ));
+                $response = new Response( $this->controller->render( 'MESDAngGridBundle:Grid:export.' . $this->grid['exportType'] . '.twig',
+                        array(
+                            'entities' => $this->grid['entities'],
+                            'headers' => $this->grid['headers'],
+                        )
+                    ) );
                 $response->headers->set( 'Content-Type', 'text/' . $this->grid['exportType'] );
                 $response->headers->set( 'Content-Disposition', 'attachment; filename="export.' . $this->grid['exportType'] . '"' );
             }
@@ -339,22 +352,22 @@ class GridManager {
 
         if ( is_null( $this->grid['exportType'] ) ) {
             $this->grid['exportLink'] = '';
-            foreach($this->grid['exportArray'] as $exType) {
+            foreach ( $this->grid['exportArray'] as $exType ) {
                 $exType['exportLink'] = '';
             }
         } else {
             $this->grid['exportLink'] = $this->controller->generateUrl( $this->exportAlias, array( 'exportType' => $this->grid['exportType'] ) ) .
-            '?exportString=true&search=' . $this->grid['search'] .
-            '&sorts=' . json_encode($this->grid['sorts']);
-            for($i = 0; $i < count($this->grid['exportArray']); $i++) {
-                $this->grid['exportArray'][$i]['exportLink'] = $this->controller->generateUrl( $this->exportAlias,
-                array( 'exportType' => $this->grid['exportArray'][$i]['value'] ) ) .
                 '?exportString=true&search=' . $this->grid['search'] .
-                '&sorts=' . json_encode($this->grid['sorts']);
+                '&sorts=' . json_encode( $this->grid['sorts'] );
+            for ( $i = 0; $i < count( $this->grid['exportArray'] ); $i++ ) {
+                $this->grid['exportArray'][$i]['exportLink'] = $this->controller->generateUrl( $this->exportAlias,
+                    array( 'exportType' => $this->grid['exportArray'][$i]['value'] ) ) .
+                    '?exportString=true&search=' . $this->grid['search'] .
+                    '&sorts=' . json_encode( $this->grid['sorts'] );
             }
         }
 
-        if ('js' == $this->grid['exportType']) {
+        if ( 'js' == $this->grid['exportType'] ) {
             $response = new JsonResponse( $this->grid );
 
             $initData = <<<EOT
@@ -371,8 +384,8 @@ gridModule.provider('initData', function() {
         return {
             initData: function() {
 EOT;
-                $initData .= 'return ' . $response->getContent();
-                $initData .= <<<EOT
+            $initData .= 'return ' . $response->getContent();
+            $initData .= <<<EOT
 ;
             }
         }
@@ -384,37 +397,37 @@ EOT;
 });
 EOT;
 
-            return new Response($initData);
+            return new Response( $initData );
         } else {
             return new JsonResponse( $this->grid );
         }
     }
 
-    public function setFormUrl($url) {
+    public function setFormUrl( $url ) {
         $this->grid['formUrl'] = $url;
     }
 
-    public function prependSearch($search){
+    public function prependSearch( $search ) {
         $this->prepend = $search[0].' ';
     }
 
     public function calculatePages() {
-            if ( 0 < $this->grid['filtered'] ) {
-                $this->grid['last'] = ceil( $this->grid['filtered'] / $this->grid['perPage'] );
-            } else {
-                $this->grid['last'] = 1;
-            }
-            if ( 1 > $this->grid['page'] ) {
-                $this->grid['page'] = 1;
-            } elseif ( $this->grid['last'] < $this->grid['page'] ) {
-                $this->grid['page'] = $this->grid['last'];
-            }
-            $this->queryBuilder->setFirstResult( $this->grid['perPage'] * ( $this->grid['page'] - 1 ) )
-            ->setMaxResults( $this->grid['perPage'] );
+        if ( 0 < $this->grid['filtered'] ) {
+            $this->grid['last'] = ceil( $this->grid['filtered'] / $this->grid['perPage'] );
+        } else {
+            $this->grid['last'] = 1;
+        }
+        if ( 1 > $this->grid['page'] ) {
+            $this->grid['page'] = 1;
+        } elseif ( $this->grid['last'] < $this->grid['page'] ) {
+            $this->grid['page'] = $this->grid['last'];
+        }
+        $this->queryBuilder->setFirstResult( $this->grid['perPage'] * ( $this->grid['page'] - 1 ) )
+        ->setMaxResults( $this->grid['perPage'] );
     }
 
     public function addSorts() {
-        if (isset($this->grid['sorts']) && '[]' != $this->grid['sorts']) {
+        if ( isset( $this->grid['sorts'] ) && '[]' != $this->grid['sorts'] ) {
             foreach ( $this->grid['sorts'] as $sort ) {
                 $this->queryBuilder->addOrderBy( $this->grid['headers'][$sort->column]['column'], $sort->direction );
                 if ( 'asc' == $sort->direction ) {
@@ -432,14 +445,14 @@ EOT;
 
     public function processResults() {
         $this->resultSet = null;
-        foreach ($this->results as $result) {
-            if (isset($result)) {
-                $class = get_class($result);
-                if ($class == $this->rootClass) {
-                    if (isset($this->resultSet)) {
+        foreach ( $this->results as $result ) {
+            if ( isset( $result ) ) {
+                $class = get_class( $result );
+                if ( $class == $this->rootClass ) {
+                    if ( isset( $this->resultSet ) ) {
                         $this->processResultSet();
                     }
-                    $this->resultSet = array('root' => $result);
+                    $this->resultSet = array( 'root' => $result );
                 } else {
                     $this->resultSet[$class][] = $result;
                 }
@@ -449,8 +462,8 @@ EOT;
     }
 
     public function processResultSet() {
-        $paths = $this->processActions('paths');
-        $buttons = $this->processActions('buttons');
+        $paths = $this->processActions( 'paths' );
+        $buttons = $this->processActions( 'buttons' );
         $values = $this->processValues();
         $this->grid['entities']['id_' . $this->resultSet['root']->getId()] = array(
             'id' => $this->resultSet['root']->getId(),
@@ -460,17 +473,17 @@ EOT;
         );
     }
 
-    public function processActions($name) {
+    public function processActions( $name ) {
         $actions = array();
-        foreach($this->grid[$name] as $action) {
-            if (isset($action['function'])) {
+        foreach ( $this->grid[$name] as $action ) {
+            if ( isset( $action['function'] ) ) {
                 $function = $action['function'];
-                $path = $function($this->resultSet, $this->controller);
-                if (isset($path['path'])) {
+                $path = $function( $this->resultSet, $this->controller );
+                if ( isset( $path['path'] ) ) {
                     $actions[$action['alias']] = $path['path'];
                 }
             } else {
-                $actions[$action['alias']] = $this->controller->generateUrl($action['alias'], array('id' => $this->resultSet['root']->getId()));
+                $actions[$action['alias']] = $this->controller->generateUrl( $action['alias'], array( 'id' => $this->resultSet['root']->getId() ) );
             }
         }
         return $actions;
@@ -478,20 +491,20 @@ EOT;
 
     public function processValues() {
         $values = array();
-        foreach($this->grid['headers'] as $header) {
-            if (isset($header['function'])) {
+        foreach ( $this->grid['headers'] as $header ) {
+            if ( isset( $header['function'] ) ) {
                 $function = $header['function'];
-                $value = $function($this->resultSet, $this->controller);
+                $value = $function( $this->resultSet, $this->controller );
                 $values[$header['column']] = $value['value'];
             } else {
                 $columns = explode( '.', $header['field'] );
                 $value = $this->resultSet['root'];
                 foreach ( $columns as $key => $column ) {
-                    if ( isset($value) && $key > 0 ) {
+                    if ( isset( $value ) && $key > 0 ) {
                         $value = call_user_func( array( $value, 'get' . ucwords( $column ) ) );
                     }
                 }
-                if (is_null($value)) {
+                if ( is_null( $value ) ) {
                     $value = '-';
                 }
                 $values[$header['column']] = $value;
@@ -502,11 +515,11 @@ EOT;
 
     public function removeHidden() {
         $columns = array();
-        foreach($this->grid['headers'] as $headerKey => $header) {
-            if ($header['hidden']) {
+        foreach ( $this->grid['headers'] as $headerKey => $header ) {
+            if ( $header['hidden'] ) {
                 $columns[] = $headerKey;
             }
         }
-        $this->hideColumns($columns);
+        $this->hideColumns( $columns );
     }
 }
