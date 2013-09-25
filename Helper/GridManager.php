6f4e29ecbb8414373deed3eeacc3cc0f6988aa7c
lighthart <dlighthart@mesd.k12.or.us>
2013-09-25 14:50:52 -0700

    Export all.  Seriously this time.

--- Helper/GridManager.php
+++ Helper/GridManager.php
@@ -363,15 +363,17 @@ class GridManager {
                 $this->grid['page'] = 1;
             }
             if ( is_null( $this->grid['perPage'] ) ) {
-                if ($this->export) {
-                    $this->grid['perPage'] = $this->grid['filtered'];
-                } else {
                     $this->grid['perPage'] = 10;
-                }
             }
+
+            if ($this->export) {
+                $this->grid['perPage'] = $this->grid['filtered'];
+            }
+
             if ( !$this->export ) {
                 $this->calculatePages();
             }
+
             $this->addSorts();
             $this->results = $this->paginator->paginate(
                 $this->queryBuilder->getQuery()->setHint( 'knp_paginator.count', $this->grid['filtered'] ),
